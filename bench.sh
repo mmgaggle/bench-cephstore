d5865a20abba4ea8c3da049a1b957eaa87b5d0fc
Kyle Bader <kyle.bader@dreamhost.com>
2013-09-30 20:15:06 -0700

    make 80col friendly

--- bench.sh
+++ bench.sh
@@ -15,7 +15,7 @@ fi
 DEVICES=$@
 
 echo " + Install packages for test prep and benchmarks"
-apt-get install -y --force-yes dmcrypt fio parted xfsprogs
+apt-get install -y --force-yes dmcrypt fio parted xfsprogs collectl
 
 for device in ${DEVICES}
 do
@@ -34,22 +34,50 @@ do
 	echo " + Mount data partition"
 	mkdir -p /mnt/${device_basename}2
 	mount ${device}2 /mnt/${device_basename}2
-	echo " + Sync system"
-	sync
-	echo " + Drop linux page cache"
-	echo 3 > /proc/sys/vm/drop_caches
+
+    # Setup collectl
+
+	echo " + FSync and drop linux page cache"
+	sync && echo 3 > /proc/sys/vm/drop_caches
     echo " -> Running 4MB random write benchmark (max rados object size)"
-    fio --rw=write --ioengine=libaio --iodepth=16 --direct=1 --numjobs 1 --runtime 300 --size 16G --bs=4M --name=${device}1 --name=/mnt/${device_basename}2
+    fio --rw=write \
+	    --ioengine=libaio \
+		--iodepth=16 \
+		--direct=1 \
+		--numjobs 1 \
+	    --runtime 300 \
+		--size 16G \
+		--bs=4M \
+		--name=${device}1 \
+		--name=/mnt/${device_basename}2
 
-	echo " + Drop linux page cache"
-	echo 3 > /proc/sys/vm/drop_caches
+	echo " + FSync and drop linux page cache"
+	sync && echo 3 > /proc/sys/vm/drop_caches
     echo " -> Running 4KB random write benchmark (small IO)"
-    fio --rw=write --ioengine=libaio --iodepth=16 --direct=1 --numjobs 1 --runtime 300 --size 16G --bs=4K --name=${device}1 --name=/mnt/${device_basename}2
+    fio --rw=write \
+	    --ioengine=libaio \
+		--iodepth=16 \
+		--direct=1 \
+		--numjobs 1 \
+	    --runtime 300 \
+		--size 16G \
+		--bs=4K \
+		--name=${device}1 \
+		--name=/mnt/${device_basename}2
 
-	echo " + Drop linux page cache"
-	echo 3 > /proc/sys/vm/drop_caches
+	echo " + FSync and drop linux page cache"
+	sync && echo 3 > /proc/sys/vm/drop_caches
     echo " -> Running 8KB random write benchmark (InnoDB IO)"
-    fio --rw=write --ioengine=libaio --iodepth=16 --direct=1 --numjobs 1 --runtime 300 --size 16G --bs=4K --name=${device}1 --name=/mnt/${device_basename}2
+    fio --rw=write \
+	    --ioengine=libaio \
+		--iodepth=16 \
+		--direct=1 \
+		--numjobs 1 \
+	    --runtime 300 \
+		--size 16G \
+		--bs=4K \
+		--name=${device}1 \
+		--name=/mnt/${device_basename}2
 
     echo "OpenSSL AES CBC benchmarks"
 	openssl speed aes-128-cbc aes-192-cbc aes-256-cbc
